a75fa0c370bb55e1608995c87d484d7b14d29b39
mitchellabrouwer <mbarron198@gmail.com>
2022-08-11 21:57:18 +1000

    chore: video types

--- types/general.ts
+++ types/general.ts
@@ -3,5 +3,6 @@ import { NextApiRequest } from "next";
 export interface Request extends NextApiRequest {
   files?: {
     image: { path: string; originalFilename: string; size: number }[];
+    video: { path: string; originalFilename: string; size: number }[];
   };
 }
