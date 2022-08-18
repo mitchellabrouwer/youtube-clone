c3277deac20419814983d2dff016ebbba419697a
mitchellabrouwer <mbarron198@gmail.com>
2022-08-18 14:00:06 +1000

    feat: begin weighting

---
+++ lib/weighting.ts
@@ -0,0 +1,8 @@
+const WEIGHTS = {
+  likes: { benefit: true, weight: 0.25 },
+  dislikes: { benefit: false, weight: 0.25 },
+  views: { benefit: true, weight: 0.15 },
+  comments: { benefit: true, weight: 0.35 },
+};
+
+export const getBestVideos = async (options, prisma) => {};
